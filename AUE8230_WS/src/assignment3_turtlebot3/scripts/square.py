e8bc9ae99b1da9621e008873621b0ba4a5eb5b19
vasudevpurohit <vasudev.purohit@gmail.com>
2022-02-14 22:44:20 -0500

    Adding the .py script for square

--- AUE8230_WS/src/assignment3_turtlebot3/scripts/square.py
+++ AUE8230_WS/src/assignment3_turtlebot3/scripts/square.py
@@ -5,9 +5,9 @@ from geometry_msgs.msg import Twist
 import math
 
 def move():
-	rospy.init_node('turtlebot2',anonymous=True)
+	rospy.init_node('turtlesim1')
 	rate = rospy.Rate(10)
-	vel_publisher = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size = 10)
+	vel_publisher = rospy.Publisher('/cmd_vel',Twist,queue_size = 10)
 	bot_vel = Twist()
 
 #finding the distance travelled by each robot based on the time elapsed
@@ -17,7 +17,7 @@ def move():
 		while (current_distance < 2):
 		    t0 = rospy.Time.now().to_sec()
 		    #linear speed = 0.2 unit/s && angular speed = 0.2 rad/s
-		    bot_vel.linear.x = 0.2
+		    bot_vel.linear.x = 0.3
 		    bot_vel.linear.y = 0
 		    bot_vel.linear.z = 0
 		    bot_vel.angular.x = 0
@@ -26,7 +26,7 @@ def move():
 		    vel_publisher.publish(bot_vel)
 		    t1 = rospy.Time.now().to_sec()
 		    #calculating the distance travelled
-		    current_distance = current_distance + 0.2*(t1-t0)
+		    current_distance = current_distance + 0.3*(t1-t0)
 
 		bot_vel.linear.x = 0
 		vel_publisher.publish(bot_vel)
@@ -35,10 +35,10 @@ def move():
 		t0 = rospy.Time.now().to_sec()
 	#turning the bot by 90deg to align with the next side
 		while (current_orientation < ((math.pi)/2)):
-		    bot_vel.angular.z = 0.2
+		    bot_vel.angular.z = 0.3
 		    vel_publisher.publish(bot_vel)
 		    t1 = rospy.Time.now().to_sec()
-		    current_orientation = 0.2*(t1-t0)
+		    current_orientation = 0.3*(t1-t0)
 
 	bot_vel.angular.z = 0
 	vel_publisher.publish(bot_vel)
