354ad059a89f97c11532242b88ffb23345888138
vasudevpurohit <vasudev.purohit@gmail.com>
2022-02-14 22:12:22 -0500

    Added a functioning circle.py file

--- AUE8230_WS/src/assignment3_turtlebot3/scripts/circle.py
+++ AUE8230_WS/src/assignment3_turtlebot3/scripts/circle.py
@@ -10,26 +10,21 @@ from geometry_msgs.msg import Twist
 
 def move():
     #initializing a node
-    rospy.init_node('turtlesim1',anonymous=True)
+    rospy.init_node('turtlesim1')
     rate = rospy.Rate(10)
     
     #publisher message
-    vel_pub = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size=10)
+    vel_pub = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
     vel_msg = Twist()
-    
-    speed = float(input("Enter the linear speed of the bot: "))
-    ang_vel = float(input("Enter the angular velocity of the bot: "))
-    #user input for speed
-
-    
+        
     #moving the robot with the set speed
     while True:
-    	vel_msg.linear.x = speed
+    	vel_msg.linear.x = 0.3
     	vel_msg.linear.y = 0
     	vel_msg.linear.z = 0
     	vel_msg.angular.x = 0
     	vel_msg.angular.y = 0
-    	vel_msg.angular.z = ang_vel
+    	vel_msg.angular.z =0.3
     
     	vel_pub.publish(vel_msg)
     	rate.sleep()
